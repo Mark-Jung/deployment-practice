b878c7397c1145d99fe8cc9022b59fc5cd31b2de
Mark Jung <gujung2022@u.northwestern.edu>
2018-11-25 00:40:02 -0600

    done

--- deployment-practice/babel.config.js
+++ deployment-practice/babel.config.js
@@ -1,6 +1,9 @@
 module.exports = function(api) {
   api.cache(true);
+  const presets = ['babel-preset-expo'];
+  const plugins = ['inline-dotenv'];
   return {
-    presets: ['babel-preset-expo'],
+    presets,
+    plugins,
   };
 };
