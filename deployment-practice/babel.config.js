d80bfa1358d39042c350c9ff51baa3851b72fde2
Mark Jung <gujung2022@u.northwestern.edu>
2018-11-25 01:23:58 -0600

    hid a line in babel.config.js

--- deployment-practice/babel.config.js
+++ deployment-practice/babel.config.js
@@ -1,7 +1,7 @@
 module.exports = function(api) {
   api.cache(true);
   const presets = ['babel-preset-expo'];
-  const plugins = ['inline-dotenv'];
+  //const plugins = ['inline-dotenv'];
   return {
     presets,
     plugins,
